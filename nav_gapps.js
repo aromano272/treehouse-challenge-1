ba58413d2170b5633dcd021cb7d3ea95950d2a4b
aromano272 <andre.romano272@gmail.com>
2014-08-05 04:29:38 +0100

    adding lightbox
    added lightbox method learnt at teamtreehouse

--- nav_gapps.js
+++ nav_gapps.js
@@ -28,6 +28,8 @@ $("body").on("click", ".fadeout", function() {
 });
 
 var fader = "<div id='fader' style='opacity: 0.5;'></div>";
+// var $overlay = $("<div id='overlay'></div>");
+// $("body").append($overlay);
 
 /*========================================
 =            CUSTOM FUNCTIONS            =
@@ -85,5 +87,57 @@ var fade_out = function() {
     $("#fader").remove();
   });
 }
+
+
+/*========================================
+=                LIGHTBOX                =
+========================================*/
+
+// add overlay
+var $overlay = $("<div id='overlay'></div>");
+$("body").append($overlay);
+// an image
+var $image = $("<img>");
+$overlay.append($image);
+// a caption
+var $caption = $("<p id='caption'></p>");
+$overlay.append($caption);
+
+// capture the click event on a link
+$("#gallery a").click(function() {
+  event.preventDefault();
+  // show the overlay
+  $overlay.show();
+
+  // update overlay with the image linked in the link
+  var imageLocation = $(this).attr("href");
+  $image.attr("src", imageLocation);
+  // get childs p element text and set caption
+  var captionText = $(this).children("p").text();
+  $caption.text(captionText);
+})
+
+
+  
+  
+// when overlay is clicked
+$overlay.click(function() {
+  // hide the overlay
+  $(this).hide("slow");
+});
+
+// stops the propagation of event, in this case the click event, up the dom tree,
+// so when we click the image of the caption is doesnt trigger the click even on $overlay
+$image.click(function() {
+  event.stopPropagation();
+});
+$caption.click(function() {
+  event.stopPropagation();
+});
+
+  
+
+
+
 
 
